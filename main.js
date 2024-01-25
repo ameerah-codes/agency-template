0f7a03ae0e8f6e74f751cab13253ccd940099f47
ameerah-codes <81390638+ameerah-codes@users.noreply.github.com>
2024-01-25 17:36:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const mobileMenu = document.querySelector ( '.mobile__menu' );
+const navMenu = document.querySelector ('.navigation');
+
+mobileMenu.addEventListener('click', () =>{
+    navMenu.classList.add('show__menu');
+});
+
+navMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show__menu')
+})
